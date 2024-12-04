ad8f098bcb9c30c8f18a2a25f4216f084037bfa3
kristazeile <116163224+kristazeile@users.noreply.github.com>
2024-12-04 12:54:37 +0200

    kkkk

---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const ap[Ur]="/api/data";
+    const fore=document.getElementById("tempForm");
+    const tableBody=document.querySelector("#dataTable tbody");
+    const avhTempElement=document.getElementById("avgTemp");
+}
